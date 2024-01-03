5940769475430197bdddd7579d0e3df201aefbc1
yichen-byte <69297535+yichen-byte@users.noreply.github.com>
2024-01-03 15:36:15 +0800

    使用fastllm进行加速

--- accelerate_demo.py
+++ accelerate_demo.py
@@ -33,29 +33,10 @@ stop_stream = False
 
 welcome_prompt = "欢迎使用中医聊天机器人，使用 clear 命令可清除聊天历史，使用 exit 命令可退出应用程序。"
 
-
-def build_prompt(query):
-    try:
-        # 构建查询URL  
-        url = f"https://www.baidu.com/sf?openapi=1&dspName=iphone&from_sf=1&pd=wenda_kg&resource_id=5243&word={query}&dsp=iphone&title={query}&aptstamp=1704029696&top=%7B%22sfhs%22%3A11%7D&alr=1&fromSite=pc&total_res_num=5011&ms=1&frsrcid=5242&frorder=5&lid=10669184107456470945&pcEqid=94108e6b002f9fa10000000365916e00"  
-        # 发送GET请求并获取响应内容  
-        response = requests.get(url)  
-        content = response.text  
-        # 使用lxml解析HTML  
-        html = etree.HTML(content)  
-        # 提取答案  
-        answers = html.xpath("/html/body/div[2]/div/div/b-superframe-body/div/div[2]/div/div/article/section/section/div/div/a/div[2]")  
-        answer_texts = {f"相似回答{i+1}": answer.text for i, answer in enumerate(answers[:5])}
-    except Exception as e:
-        answer_texts = "无相似回答"
-
-    # 构建prompt
-    prompt = f'现在你是一名专业的中医医生，请回答以下患者的问诊问题：“{query}"，这里有一些相似的回答可能会帮助到你，注意请以你的知识为主，相似回答仅作为参考。相似回答：'
-    print(answer_texts)
-    return prompt
-
 def main():
     past_key_values, history = None, []
+    history = [('现在你是一名专业的中医医生，请用你的专业知识提供详尽而清晰的关于中医问题的回答。', '当然，我将尽力为您提供关于中医的详细而清晰的回答。请问您有特定的中医问题或主题感兴趣吗？您可以提出您想了解的中医相关问题，比如中医理论、诊断方法、治疗技术、中药等方面的问题。我将根据您的需求提供相应的解答。')]
+
     global stop_stream
     print(welcome_prompt)
     while True:
@@ -68,7 +49,6 @@ def main():
             print(welcome_prompt)
             continue
         print("\n医师：", end="")
-        query = build_prompt(query)
         current_length = 0
         for response, history, past_key_values in model.stream_chat(tokenizer, query, history=history, top_p=1,
                                                                     temperature=0.01,
